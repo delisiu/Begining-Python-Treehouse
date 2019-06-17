d4a1633c7cf6f35681d27075f243287e4c2df8e6
delisiu <51231054+delisiu@users.noreply.github.com>
2019-06-17 10:44:14 +0200

    Update Basics.py

--- Classes/Basics.py
+++ Classes/Basics.py
@@ -3,3 +3,25 @@ class Student:
     
 me = Student()
 print(me.name)
+
+
+class Student:
+    name = "Mac"
+    def praise (self):
+        return ("You're doing a great job {}".format(self.name))
+
+
+    
+class Student:
+    name = "Your Name"
+    
+    def praise(self):
+        return "You inspire me, {}".format(self.name)
+    
+    def reassurance(self):
+        return "Chin up, {}. You'll get it next time!".format(self.name)
+    
+    def feedback(self, grade):
+        if grade > 50:
+            return self.praise()
+        return self.reassurance()
